c55e7b1f774ec72b9ef4009b77fb160aaf164523
glewis67 <84549006+glewis67@users.noreply.github.com>
2021-12-28 18:33:18 -0600

    Added the skeleton of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React,{useState}from"react"
 import './App.css';
-
+import Nav from "./components/Nav"
+import Page from "./components/Page"
+import Footer from "./components/Footer"
 function App() {
+  const [pages]=useState([
+    {name:"about"},
+    {name:"projects"},
+    {name:"contact"},
+    {name:"resume"},
+  ])
+  const[
+    currentPage,setCurrentPage
+  ]=useState(pages[0])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Nav
+      pages={pages}
+      setCurrentPage={setCurrentPage}
+      currentPage={currentPage}
+     ></Nav>
+     <Page currentPage={currentPage}></Page>
+     <Footer></Footer> 
     </div>
   );
 }
